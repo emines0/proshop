2553cb3a6f1655306922a93ee8d0596d9114514e
emines0 <emin.sefa1@gmail.com>
2023-11-25 20:54:58 +0100

    Raitngs components

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,4 +1,6 @@
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
   return (
@@ -6,22 +8,26 @@ const Product = ({ product }) => {
     // The "my-3" class is a Bootstrap class that adds margin to the top and bottom of an element
     // The "p-3" class is a Bootstrap class that adds padding to all sides of an element
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img
           src={product.image}
           variant="top"
         />
-      </a>
+      </Link>
+
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
+
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          {/* The Rating component is used to display the rating of a product */}
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
