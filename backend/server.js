0732415a82079ac0908237ee19d175f860133569
emines0 <emin.sefa1@gmail.com>
2023-11-26 12:30:26 +0100

    install nodemon, concurrently

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+// dotenv.config() loads the variables from .env file into process.env
+dotenv.config();
+import products from './data/products.js';
 
-const port = 5002;
+const port = process.env.PORT || 5002;
 
 // Initialize express
 const app = express();
@@ -8,6 +12,18 @@ const app = express();
 // Create a route
 app.get('/', (req, res) => {
   res.send('API is running....');
+});
+
+// Create a route for products
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+// Create a route for single product
+app.get('/api/products/:id', (req, res) => {
+  // If the id of the product matches the id in the url, return the product
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 // Listen to port
